2ab5b554a7e8546d6e26888f23ee5feb63ea989c
GustavH <gkhaavik@gmail.com>
2024-10-04 22:55:47 +0200

    Cleanup UserService Service

--- UserService/src/main/java/com/gostavdev/commercify/userservice/service/UserService.java
+++ UserService/src/main/java/com/gostavdev/commercify/userservice/service/UserService.java
@@ -2,31 +2,18 @@ package com.gostavdev.commercify.userservice.service;
 
 import com.gostavdev.commercify.userservice.UserRepository;
 import com.gostavdev.commercify.userservice.entities.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    public User registerUser(User user) {
-        // Encode the password before saving
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
-    }
-
-    public User createUser(User user) {
-        return userRepository.save(user);
-    }
 
     public User getUserById(Long id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new BadCredentialsException("No user found with id: " + id));
     }
 }

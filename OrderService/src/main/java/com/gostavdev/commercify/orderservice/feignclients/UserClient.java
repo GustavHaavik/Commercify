99239c9261878a37bb450ac1fbec3785009a285d
GustavH <gkhaavik@gmail.com>
2024-10-05 13:26:59 +0200

    OrderService:  - Removes unused imports  - Changes ddl-auto to update

--- OrderService/src/main/java/com/gostavdev/commercify/orderservice/feignclients/UserClient.java
+++ OrderService/src/main/java/com/gostavdev/commercify/orderservice/feignclients/UserClient.java
@@ -1,6 +1,5 @@
 package com.gostavdev.commercify.orderservice.feignclients;
 
-import com.gostavdev.commercify.orderservice.dto.ProductDto;
 import com.gostavdev.commercify.orderservice.dto.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
